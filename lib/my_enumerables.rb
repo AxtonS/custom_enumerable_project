f8ad0bf1003ed4515cfaf7a62d92c9d0d5e846f2
Axton Smith <axtonsmith@gmail.com>
2022-07-15 15:47:36 -0400

    Finish my_each_with_index method in Enumerable module

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 # Custome enumerable functions
-module Enumerable; end
+module Enumerable
+  def my_each_with_index
+    array = []
+    i = 0
+    while i < length
+      array.push(self[i])
+      yield(self[i], i)
+      i += 1
+    end
+    array
+  end
+end
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
