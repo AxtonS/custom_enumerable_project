35e337cc2f289bf1a4f66475f00eb040372d451b
Axton Smith <axtonsmith@gmail.com>
2022-07-15 03:13:13 -0400

    Wrote logic that can individually pass both #my_each tests

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,25 @@
-module Enumerable
-  # Your code goes here
-end
+# frozen_string_literal: true
+
+# Custome enumerable functions
+module Enumerable; end
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    array = []
+    while i < length
+      
+      # This line passes "returns the original array" test
+      # array.push(self[i])
+      
+      # This line passes "executes the block for each element" test
+      # array.push(yield(self[i]))
+      i += 1
+    end
+    array
+  end
 end
